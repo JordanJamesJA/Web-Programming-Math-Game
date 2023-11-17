7fdc062f465d833f36ca506735a7424d5315be87
JordanJamesJA <97801730+JordanJamesJA@users.noreply.github.com>
2023-11-17 00:19:02 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -106,10 +106,89 @@ function registerAndDisableFields() {
   return registrationResult;
 }
 
+//Task 4
+
+//should rename this to PlayGame()
 function startFunction() {
   alert("Start button clicked! :)");
-}
 
+  //enables the buttons in the play area
+  document.getElementById("check").disabled = false;
+  document.getElementById("next").disabled = false;
+  document.getElementById("accept").disabled = false;
+    
+  
+  //stores the user's answer
+    
+  
+    var userans = document.getElementById("ans").value;
+
+    //the 2 random numbers (1-9) & (1-5)
+    let num1 = Math.floor(Math.random() * 9) + 1;
+    let num2 = Math.floor(Math.random() * 5) + 1;
+    
+    //displays the two numbers in disabled fields
+    document.getElementById("randnum1").value = num1;
+    document.getElementById("randnum2").value = num2;
+    
+    
+    
+    checkAns();
+    }
+    
+    function checkAns(){
+      if (
+        userans === "")
+        {
+          alert("No answer was entered");
+          return false;
+        }
+      //idk why this doesn't work
+
+    //checking to see if the random number is 1-5 for the correct answer
+    if (num2 === 1)
+    {
+    correctans = num1;
+    }
+    
+    if (num2 === 2)
+    {
+    correctans = num1*2;
+    } 
+    
+    if (num2 === 3)
+    {
+    correctans = num1*3;
+    }
+    
+    if (num2 === 4)
+    {
+    correctans = num1*4;
+    }
+    
+    if (num2 === 5)
+    {
+    correctans = num1*5;
+    }
+    
+    
+   //check to see if the answer is correct
+    if (userans === correctans) 
+    {
+      return document.getElementById("result").value = "Correct!";
+      //push the response to PlayerRegistrationData
+    } 
+    else 
+    {
+      return document.getElementById("result").value = "Incorrect!";
+      //push the response to PlayerRegistrationData
+    }
+    
+
+    }
+
+
+// rename to findPercentageScore()
 function endFunction() {
   alert("End button clicked! :)");
 }
